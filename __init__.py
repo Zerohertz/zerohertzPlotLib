7d0a58680555020b68d79df61e2a981c37bec5a3
Zerohertz <ohg3417@gmail.com>
2023-03-13 10:49:33 +0900

    :alarm_clock: Add: Mean Processing Time

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .meanProcessingTime import meanProcessingTime
---
+++ meanProcessingTime.py
@@ -0,0 +1,30 @@
+import os
+import csv
+import pandas as pd
+
+'''
+A function that averages the processing time of the model
+according to each input measured through functions such as time.time()
+
+[sec] -> [msec]
+
+├── Model1.csv
+├── Model2.csv
+├── Model3.csv
+├── ...
+└── ModelN.csv
+
+-> time.csv
+'''
+
+def meanProcessingTime():
+    if 'time.csv' in os.listdir():
+        os.remove('./time.csv')
+
+    for i in os.listdir():
+        if '.csv' in i and not i == 'time.csv':
+            data = pd.read_csv(i, header=None)
+            tmp = data.mean() * 1000
+            with open('./time.csv', 'a', encoding='utf8') as f:
+                wr = csv.writer(f)
+                wr.writerow([i[0:-4], *tmp])
