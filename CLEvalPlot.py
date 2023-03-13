eda9dbc58a9b208cf256ea53bdd427d8f8b07f7f
zerohertz <ohg3417@gmail.com>
2023-03-13 18:00:23 +0900

    :fire: Update: CLEval Plot

--- CLEvalPlot.py
+++ CLEvalPlot.py
@@ -83,19 +83,25 @@ def CLEvalPlot(time_dir, figname='', fontsize=20, fontfamily='Times New Roman',
     plt.ylabel('Time [ms]')
     plt.legend(loc='upper right', bbox_to_anchor=(1.21, 1.02))
     os.chdir(org)
-    MetName = ['HMean', 'Precision', 'Recall']
+    res = [[] for _ in range(4)]
+    ro = 3
     for i in range(3):
-        print('='*20, '\t', MetName[i], '\t', '='*20)
-        print(Ver[0], ': ', Met[i][0], '[%]')
-        print(Ver[-1], ': ', Met[i][-1], '[%]')
-        print(Met[i][-1] - Met[i][0], '[%p]')
-        print((Met[i][-1] - Met[i][0]) / Met[i][0] * 100, '[%]')
-    print('='*20, '\tTime\t', '='*20)
-    print(Ver[0], ': ', Time[5][0], '[ms]')
-    print(Ver[-1], ': ', Time[5][-1], '[ms]')
-    print(Time[5][-1] - Time[5][0], '[ms]')
-    print((Time[5][-1] - Time[5][0]) / Time[5][0] * 100, '[%]')
-    print('='*20, '\tEnd\t', '='*20)
+        res[i].append('{:.3f} [%]'.format(Met[i][0]))
+        res[i].append('{:.3f} [%]'.format(Met[i][-1]))
+        res[i].append('{:.3f} [%p]'.format(Met[i][-1] - Met[i][0], 2))
+        res[i].append('{:.3f} [%]'.format((Met[i][-1] - Met[i][0]) / Met[i][0] * 100, 2))
+    res[3].append('{:.3f} [ms]'.format(Time[5][0]))
+    res[3].append('{:.3f} [ms]'.format(Time[5][-1]))
+    res[3].append('{:.3f} [ms]'.format(Time[5][-1] - Time[5][0]))
+    res[3].append('{:.3f} [%]'.format((Time[5][-1] - Time[5][0]) / Time[5][0] * 100))
+    print('||HMean|Precision|Recall|Time|')
+    print('|:-:|:-:|:-:|:-:|:-:|')
+    col = [Ver[0], Ver[-1], 'Difference', 'Percentage']
+    for i in range(len(res[0])):
+        print('|' + col[i] + '|', end='')
+        for j in range(len(res)):
+            print(res[j][i], end='|')
+        print()
     if figname != '':
         print("Saving...")
         plt.savefig(figname + '.png', dpi=300, bbox_inches='tight', pad_inches=0.3, transparent=False)
