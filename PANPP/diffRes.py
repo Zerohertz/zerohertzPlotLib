b3e989c1032cd0d9a3b524a2a56f10fafe553b81
zerohertz <ohg3417@gmail.com>
2023-04-05 16:51:08 +0900

    :recycle: Update: More Subplots

--- PANPP/diffRes.py
+++ PANPP/diffRes.py
@@ -25,23 +25,24 @@ def diffRes(output_dir, img, cut, Ver, figname='',
     '''
     plt.rcParams['font.size'] = fontsize
     plt.rcParams['font.family'] = fontfamily
-    if 4 < len(Ver) or len(Ver) <= 1:
-        return
-    else:
-        fig = plt.figure(figsize=(15, 15))
-        fig.set_facecolor('white')
-        for i, j in enumerate(Ver):
-            if len(Ver) == 2:
-                plt.subplot(1,2,i+1)
-            else:
-                plt.subplot(2,2,i+1)
-            tmp = plt.imread(output_dir + '/' + j + '/' + img)
-            if cut == []:
-                plt.imshow(tmp)
-            else:
-                plt.imshow(tmp[cut[0]:cut[0]+cut[2],cut[1]:cut[1]+cut[3],:])
-            plt.title(j.replace('_', ' '))
-            plt.axis('off')
-        if figname != '':
-            plt.savefig(figname + '.png', dpi=res, bbox_inches='tight', pad_inches=0.3, transparent=False)
-            plt.close('all')
+    fig = plt.figure(figsize=(15, 15))
+    fig.set_facecolor('white')
+    for i, j in enumerate(Ver):
+        if len(Ver) == 2:
+            plt.subplot(1,2,i+1)
+        elif 2 < len(Ver) <= 4:
+            plt.subplot(2,2,i+1)
+        elif 4 < len(Ver) <= 6:
+            plt.subplot(3,2,i+1)
+        else:
+            return
+        tmp = plt.imread(output_dir + '/' + j + '/' + img)
+        if cut == []:
+            plt.imshow(tmp)
+        else:
+            plt.imshow(tmp[cut[0]:cut[0]+cut[2],cut[1]:cut[1]+cut[3],:])
+        plt.title(j.replace('_', ' '))
+        plt.axis('off')
+    if figname != '':
+        plt.savefig(figname + '.png', dpi=res, bbox_inches='tight', pad_inches=0.3, transparent=False)
+        plt.close('all')
